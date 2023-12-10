8bea07a573fb79b9f184b38a8e7a06bbb57904f8
venson999 <venson999@126.com>
2023-12-10 20:34:28 +0800

    do some formatting

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,15 +1,15 @@
 return {
   {
-    event = "VeryLazy",
     "tpope/vim-fugitive",
+    event = "VeryLazy",
     cmd = "Git",
     config = function()
       vim.cmd.cnoreabbrev([[git Git]])
     end,
   },
   {
-    event = "VeryLazy",
     "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
     config = function()
       require("gitsigns").setup()
     end,
@@ -23,18 +23,18 @@ return {
     event = "VeryLazy",
     config = function()
       vim.cmd([[
-	" disable the default highlight group
-	let g:conflict_marker_highlight_group = ''
+    	  " disable the default highlight group
+	      let g:conflict_marker_highlight_group = ''
 
-	" Include text after begin and end markers
-	let g:conflict_marker_begin = '^<<<<<<< .*$'
-	let g:conflict_marker_end   = '^>>>>>>> .*$'
+      	" Include text after begin and end markers
+	      let g:conflict_marker_begin = '^<<<<<<< .*$'
+	      let g:conflict_marker_end   = '^>>>>>>> .*$'
 
-	highlight ConflictMarkerBegin guibg=#2f7366
-	highlight ConflictMarkerOurs guibg=#2e5049
-	highlight ConflictMarkerTheirs guibg=#344f69
-	highlight ConflictMarkerEnd guibg=#2f628e
-	highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+	      highlight ConflictMarkerBegin guibg=#2f7366
+	      highlight ConflictMarkerOurs guibg=#2e5049
+	      highlight ConflictMarkerTheirs guibg=#344f69
+	      highlight ConflictMarkerEnd guibg=#2f628e
+	      highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
       ]])
     end,
   },
