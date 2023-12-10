65998977cb90a43fe59b1e53c242cbe888e1cda6
venson999 <venson999@126.com>
2023-12-10 15:00:06 +0800

    replace nerdtree with nvimtree and replace persistence with auto-sesssion

--- init.lua
+++ init.lua
@@ -9,9 +9,3 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     })
   end,
 })
-
-local args = vim.api.nvim_get_vvar("argv")
-if #args > 2 then
-else
-  require("persistence").load({ last = true })
-end
