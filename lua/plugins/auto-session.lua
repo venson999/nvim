017cf00a37349483c8c3bc747410344610a8b711
venson999 <venson999@126.com>
2023-12-13 18:42:40 +0800

    tweak auto-session configuration

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,23 +1,29 @@
 return {
   "rmagatti/auto-session",
-  dependencies = { "venson999/auto-session-nvim-tree" },
+  dependencies = {
+    "venson999/auto-session-nvim-tree",
+    "nvim-telescope/telescope.nvim",
+  },
   config = function()
     local auto_session = require("auto-session")
     local auto_session_nvim_tree = require("auto-session-nvim-tree")
-
     auto_session.setup({
       log_level = "error",
+      auto_session_enabled = true,
       cwd_change_handling = {
-        restore_upcoming_session = true,
+        restore_upcoming_session = true, -- This is necessary for auto-session-nvim-tree
       },
-      auto_restore_enabled = false,
       auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
     })
     auto_session_nvim_tree.setup(auto_session)
 
     local keymap = vim.keymap
-
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+    keymap.set("n", "<leader>ss", require("auto-session.session-lens").search_session, { noremap = true })
   end,
 }
