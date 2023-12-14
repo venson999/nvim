8fd6236ad4d5a2675d6ba1eab30c2c2956873fee
venson999 <venson999@126.com>
2023-12-14 11:38:38 +0800

    tweak bufferline configuration

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,11 +1,25 @@
 return {
   "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.opt.termguicolors = true
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        mode = "buffers",
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text_align = "center",
+          },
+        },
+        separator_style = "slant",
+      },
+    })
+
+    local keymap = vim.keymap
+    keymap.set("n", "<S-h>", ":BufferLineCyclePrev<cr>", { desc = "Go to previous tab", noremap = true, silent = true })
+    keymap.set("n", "<S-l>", ":BufferLineCycleNext<cr>", { desc = "Go to next tab", noremap = true, silent = true })
+  end,
 }
