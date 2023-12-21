5d1c088002fc511348607f00c0279cc4e6c73f0a
venson999 <venson999@126.com>
2023-12-21 10:16:49 +0800

    format code

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -34,51 +34,18 @@ return {
         r = { "<cmd>Telescope oldfiles<cr>", "Fuzzy find recent files" },
         s = { "<cmd>Telescope live_grep<cr>", "Find string in cwd" },
       },
+      -- stylua: ignore start
       h = {
         name = "harpoon",
-        a = {
-          function()
-            require("harpoon"):list():append()
-          end,
-          "Add to the list",
-        },
-        h = {
-          function()
-            require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-          end,
-          "Toggle the list",
-        },
-        ["1"] = {
-          function()
-            require("harpoon"):list():select(1)
-          end,
-          "Switch to the 1th file",
-        },
-        ["2"] = {
-          function()
-            require("harpoon"):list():select(2)
-          end,
-          "Switch to the 2th file",
-        },
-        ["3"] = {
-          function()
-            require("harpoon"):list():select(3)
-          end,
-          "Switch to the 3th file",
-        },
-        ["4"] = {
-          function()
-            require("harpoon"):list():select(4)
-          end,
-          "Switch to the 4th file",
-        },
-        ["5"] = {
-          function()
-            require("harpoon"):list():select(5)
-          end,
-          "Switch to the 5th file",
-        },
+        a = { function() require("harpoon"):list():append() end, "Add to the list" },
+        h = { function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, "Toggle the list" },
+        ["1"] = { function() require("harpoon"):list():select(1) end, "Switch to the 1th file" },
+        ["2"] = { function() require("harpoon"):list():select(2) end, "Switch to the 2th file" },
+        ["3"] = { function() require("harpoon"):list():select(3) end, "Switch to the 3th file" },
+        ["4"] = { function() require("harpoon"):list():select(4) end, "Switch to the 4th file" },
+        ["5"] = { function() require("harpoon"):list():select(5) end, "Switch to the 5th file" },
       },
+      -- stylua: ignore end
       s = {
         name = "session",
         a = { "<cmd>SessionManager save_current_session<cr>", "Save current session" },
