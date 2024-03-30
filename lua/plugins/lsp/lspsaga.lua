951b5d5a53de3a40f6cfe4374b655c777d8d32f7
venson999 <venson999@126.com>
2024-03-30 19:25:27 +0800

    delete lspsaga

--- lua/plugins/lsp/lspsaga.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvimdev/lspsaga.nvim",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {},
-}
