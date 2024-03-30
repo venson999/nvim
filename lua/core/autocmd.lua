04ffbb34bb4f2c2f797dc9f700e260ca6dca5898
venson999 <venson999@126.com>
2024-03-30 19:27:11 +0800

    close some filetypes with <q>

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -17,3 +17,27 @@ vim.api.nvim_create_autocmd("SessionLoadPost", {
     require("neo-tree.command").execute({ action = "show" })
   end,
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "notify",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
