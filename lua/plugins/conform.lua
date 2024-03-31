e65567e13de3f5ee2ba79e50ea51882f405d5b1e
venson999 <venson999@126.com>
2024-03-31 17:11:37 +0800

    add nvim-lint add python support

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,23 +7,19 @@ return {
       conform.setup({
         formatters_by_ft = {
           lua = { "stylua" },
-          -- Conform will run multiple formatters sequentially
-          --python = { "isort", "black" },
-          -- Use a sub-list to run only the first available formatter
           javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
           json = { "prettier" },
           jsonc = { "prettier" },
+          python = { "isort" },
         },
         format_on_save = {
           -- These options will be passed to conform.format()
           timeout_ms = 500,
           lsp_fallback = true,
           async = false,
-        },
-        formatters = {
-          prettier = {
-            prepend_args = { "--no-semi" },
-          },
         },
       })
     end,
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -32,7 +32,7 @@ return {
         -- "graphql",
         "emmet_ls",
         -- "prismals",
-        -- "pyright",
+        "pyright",
       },
       automatic_installation = true, -- not the same as ensure_installed
     })
@@ -41,9 +41,8 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        -- "isort", -- python formatter
-        -- "black", -- python formatter
-        -- "pylint", -- python linter
+        "isort", -- python formatter
+        "pylint", -- python linter
         "eslint_d", -- js linter
       },
     })
